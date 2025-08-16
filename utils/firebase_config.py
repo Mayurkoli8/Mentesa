243f629486dcc2a96355e89e7e257a264cfb9829
mayurkoli8 <kolimohit9595@gmail.com>
2025-08-17 04:39:30 +0530

    bots persists

--- utils/firebase_config.py
+++ utils/firebase_config.py
@@ -1,8 +1,12 @@
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
+import streamlit as st
 
-# Path to your service account JSON
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
+# Load JSON from Streamlit secrets
+service_account_info = json.loads(st.secrets["FIREBASE"]["SERVICE_ACCOUNT_JSON"])
+
+cred = credentials.Certificate(service_account_info)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
