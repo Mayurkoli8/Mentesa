8ab24fa6c1ea32e94e1ade777526635b6ac27b02
mayurkoli8 <kolimohit9595@gmail.com>
2025-09-07 17:39:24 +0530

    deleted useless files

--- encoder.py
+++
@@ -1,9 +0,0 @@
-import json
-import base64
-
-with open("serviceAccountKey.json") as f:
-    data = json.load(f)
-
-encoded = base64.b64encode(json.dumps(data).encode("utf-8")).decode("utf-8")
-
-print(encoded)
