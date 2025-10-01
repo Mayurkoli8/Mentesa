cb7e53889ee9fa51a0af3da3424dff7b236b8986
mayurkoli8 <kolimohit9595@gmail.com>
2025-10-02 01:27:05 +0530

    removing stremlit footer and header

--- frontend/ui.py
+++ frontend/ui.py
@@ -55,12 +55,3 @@ def apply_custom_styles():
 }
 </style>
     """, unsafe_allow_html=True)
-
-hide_st_style="""
-    <style>
-    #MainMenu {visibility: hidden;}
-    footer {visibility: hidden;}
-    header {visibility: hidden;}
-    </style>
-    """
-st.markdown(hide_st_style, unsafe_allow_html=True)
