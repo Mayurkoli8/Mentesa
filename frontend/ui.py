215f0d67dc49ad0ea5202f449a5ff2d17300e896
mayurkoli8 <kolimohit9595@gmail.com>
2025-10-04 04:47:38 +0530

    removing the header

--- frontend/ui.py
+++ frontend/ui.py
@@ -53,14 +53,17 @@ def apply_custom_styles():
     color: #e2e8f0;
     border: 1px solid #334155;
 }
+#MainMenu {visibility: hidden;}
+header {visibility: hidden;}
+footer {visibility: hidden;}
 </style>
     """, unsafe_allow_html=True)
 
-hide_header = """
-    <style>
-    footer {visibility: hidden;}
-    #MainMenu {visibility: hidden;}
-    header {visibility: hidden;}
-    </style>
-"""
-st.markdown(hide_header, unsafe_allow_html=True)
+# hide_header="""
+#     <style>
+#     footer {visibility: hidden;}
+#     #MainMenu {visibility: hidden;}
+#     header {visibility: hidden;}
+#     </style>
+# """
+# st.markdown(hide_header, unsafe_allow_html=True)
